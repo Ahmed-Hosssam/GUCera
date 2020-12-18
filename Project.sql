83e37ee9042f7ecfa921df4f3e256061d314bf37
Ahmed hossam <toxin435@gmail.com>
2020-12-19 00:41:19 +0200

    Added new Tables

--- Project.sql
+++ Project.sql
@@ -1,134 +1,44 @@
-﻿create database GUCera;
-
-
-
-
-
-create table Users
+﻿Create TABLE StudentTakeAssignment
 (
-    id int identity,
-    firstName varchar(20),
-    lastName varchar(20),
-    password varchar(20),
-    gender bit,
-    address varchar(10)
-)
-
-create table Instructor
-(
-    uid int,
-    rating int,
-    FOREIGN Key (uid) references Users,
-    PRIMARY KEY (uid)
-)
-
-create table UserMobileNumber 
-(
-    uid int,
-    mobileNumber varchar(20),
-    FOREIGN key (uid) references Users,
-    PRIMARY key (uid, mobileNumber)
-)
-
-create table Student
-(
-    uid int,
-    gpa real,
-    Primary key (id),
-    foreign key (id) references Users
-)
-
-create table Admin
-(
-    uid int,
-    primary key (uid),
-    
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-create table Feedback (
-    cid int ,
-    number  int ,
-    ommentvarchar (100) ,
-    numberOfLint ,
-    sid ikes s c
-)  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Create TABLE StudentTakeAssignmentsi(d
- in t
- ,
-    ci idnt,
-    
-       in int,
-    t cid,
-    assignmentTaype vrchar ,
-    grade,
+    sid              int,
+    cid              int,
+    assignmentNumber int,
+    assignmentType   varchar,
+    grade            decimal(5,2),
     FOREIGN Key (sid) references Student,
-    FOREIGN Key (cid) references ASSIGNMENT,
-    FOREIGN Key (assignmentNumber) references Student,
-    FOREIGN Key (sid) references Student, real   int sid,
+    FOREIGN Key (cid,assignmentNumber,assignmentType) references ASSIGNMENT,
+    PRIMARY KEY (sid, cid, assignmentNumber, assignmentType, grade)
 )
+CREATE TABLE StudentRateInstructor
+(
+    sid    int,
+    instId int,
+    rate   int,
+    PRIMARY KEY (sid, instId)
+)
+CREATE TABLE StudentCertifyCourse
+(
+    sid int,
+    cid int ,
+    issueDate DATETIME,
+    FOREIGN KEY (sid) REFERENCES Student,
+    foreign key (cid) REFERENCES Course,
+    PRIMARY KEY (sid,cid)
+ )
+ CREATE TABLE CoursePrerequisiteCourse(
+     cid int,
+     prerequisiteId int,
+     FOREIGN KEY (cid,prerequisiteId) REFERENCES Course,
+     primary KEY  (cid,prerequisiteId)
+ )
+ CREATE TABLE InstructorTeachCourse(
+     instId int,
+     cid int,
+     FOREIGN KEY (instId) REFERENCES Instructor,
+     FOREIGN KEY (cid) REFERENCES Course,
+     PRIMARY KEY (instId,cid) 
+ )
+
 
 
 
